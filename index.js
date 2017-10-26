b8246f525be635d23999df2278780c912f972b97
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-26 04:20:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+  return querySelector(selector).innerHTML
+}
+function nestedTarget(){
+  return querySelector('div#nested div').target
+}
+function increaseRankBy(n){
+  var ranks = querySelectorAll('ul.ranked-list')
+  for(var i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML += 1
+  }
+}
+function deepestChild(){
+  
+}
